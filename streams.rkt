ca4cd494e928feea109b4112273c8b04f0e14249
Harry <hjwp2@cantab.net>
2020-12-30 02:22:14 +0000

    streams working ok ok

--- streams.rkt
+++ streams.rkt
@@ -54,3 +54,96 @@
 
 (display "sum-primes 1-1oo: ")
 (sum-primes 1 100)
+
+; dont do this its slow
+;(car (cdr (filter prime?
+;                 (enumerate-interval 10000 1000000))))
+
+; --------- STREAM STUFF BEGINS IN EARNEST ---------
+
+(define (stream-car stream) (car stream))
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map-old proc s)
+  (if (stream-null? s)
+      the-empty-stream
+      (cons-stream (proc (stream-car s))
+                   (stream-map-old proc (stream-cdr s)))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (display-line x)
+  (newline)
+  (display x))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+       low
+       (stream-enumerate-interval (+ low 1) high))))
+
+(define (memo-proc proc)
+  (let ((already-run? false) (result false))
+    (lambda ()
+      (if (not already-run?)
+          (begin (set! result (proc))
+                 (set! already-run? true)
+                 result)
+          result))))
+
+(define (delay exp) (memo-proc (lambda () exp)))
+
+(display "this should be 10009: ")
+(stream-car
+ (stream-cdr
+  (stream-filter prime?
+                 (stream-enumerate-interval 10000 1000000))))
+
+; Exercise 3.50.  Complete the following definition,
+; which generalizes stream-map to allow procedures that take multiple arguments,
+; analogous to map in section 2.2.3, footnote 12.
+
+(define (stream-map proc . argstreams)
+  (if (null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
+
+(define addedstreams
+  (stream-map
+    (lambda (x y) (+ x y))
+    (stream-enumerate-interval 100 100000)
+    (stream-enumerate-interval 300 300000)
+    ))
+(display "this should be 402: ")
+(stream-car (stream-cdr addedstreams))
+
+(define singleargsstillworks
+  (stream-map
+    (lambda (x) (* x 2))
+    (stream-enumerate-interval 100 100000)))
+(display "this should be 202: ")
+(stream-car (stream-cdr singleargsstillworks))
