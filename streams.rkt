8fff92d068e4e5e98c770153b450d3d390294f2c
Harry <hjwp2@cantab.net>
2021-01-05 15:52:00 +0000

    moar on streams

--- streams.rkt
+++ streams.rkt
@@ -3,7 +3,7 @@
 (display "--------------------------------------------")
 (newline)
 
-; some stuff from earlier chapters:
+; -- some stuff from earlier chapters --
 (define (square x) (* x x))
 
 ; primes
@@ -59,6 +59,7 @@
 ;(car (cdr (filter prime?
 ;                 (enumerate-interval 10000 1000000))))
 
+
 ; --------- STREAM STUFF BEGINS IN EARNEST ---------
 
 (define (stream-car stream) (car stream))
@@ -113,25 +114,14 @@
           result))))
 
 ; ---------------------------
-; HERE I GOT CONFUDDLED
+; TODO: further investigation required
 
+; this won't work bc delay has to be a special form I think???
+; (define (delay exp) (lambda () exp))
 ; (define (delay exp) (memo-proc (lambda () exp)))
 ; 
-; (define myvar 1001)
-; (define testmoz (lambda (x) (set! myvar (+ myvar x))))
-; (define dtestmoz (delay testmoz))
-; (define (force p) (p))
-; (force dtestmoz)
-; (force dtestmoz)
-; (force dtestmoz)
-; (force dtestmoz)
-; (display "this should show 1002:")
-; (display myvar)
-; (newline)
+; ya bc otherwise exp would be evaluated immediately or sumfink
 
-; the
-;(define (delay exp) (memo-proc (lambda () exp)))
-; (define (delay exp) (lambda () exp))
 ; ---------------------------
 
 (display "this should be 10009: ")
@@ -205,14 +195,140 @@
   (if (null? (stream-car stream)) the-empty-stream
     (let ((first (stream-car stream)))
       (if (pred first) (cons first (takewhile pred (stream-cdr stream)))
+        ; takewhile  !=  filter.  once pred is false we just bail
         the-empty-stream))))
 
 (display "this should show 1-99 ")
 (takewhile (lambda (x) (< x 100)) integers)
 
+(newline)
 
 
+;(define (fibgen a b)
+; (cons-stream a (fibgen b (+ a b))))
+
+(define (fibs-starting-from n-2 n-1)
+  (let ((n (+ n-2 n-1)))
+    (cons-stream n-2 (fibs-starting-from n-1 n))))
+(define fibs (fibs-starting-from 0 1))
+
+(display "this should print fibs up to 34 55 89: ")
+(takewhile (lambda (x) (< x 100)) fibs)
+
+
+;; -- sieve of Eratosthenes --
+
+(define (divisible? x y) (= (remainder x y) 0))
+
+(define (sieve stream)
+  (cons-stream
+   (stream-car stream)
+   (sieve (stream-filter
+           (lambda (x)
+             (not (divisible? x (stream-car stream))))
+           (stream-cdr stream)))))
+
+(define primes (sieve (integers-starting-from 2)))
+
+(define (take-n stream n) 
+  (cond
+  ((null? (stream-car stream)) the-empty-stream)
+  ((= n 0) the-empty-stream)
+  (else
+    (cons (stream-car stream) (take-n (stream-cdr stream) (- n 1))))))
+
+(display "this should print the first 10 primes: ")
+(take-n primes 10)
+
+(define (scale-stream stream factor)
+  (stream-map (lambda (x) (* x factor)) stream))
+
+; mindblown, this feels like it should be just multiples of 2 but its powers of 2
+(define double (cons-stream 1 (scale-stream double 2)))
+(display "this should print powers of 2 (!): ")
+(take-n double 10)
+
+(display "this should print multiples of 2: ")
+(define multiples-of-2 (scale-stream integers 2))
+(take-n multiples-of-2 10)
+
+; ok that makes sense
+
+; --- moar infinite streaaaaaams ----
+(define ones (cons-stream 1 ones))
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+(define integers2 (cons-stream 1 (add-streams ones integers2)))
+
+; Exercise 3.53.  Without running the program, describe the elements of the
+; stream defined by
+(define s (cons-stream 1 (add-streams s s)))
+(display "this should print 1, 2, 4, 8, 16...: ")
+(take-n s 10)
+
+; Exercise 3.54.  Define a procedure mul-streams, analogous to add-streams,
+; that produces the elementwise product of its two input streams.
+; Use this together with the stream of integers to complete 
+; the following definition of the stream whose nth element (counting from 0)
+; is n + 1 factorial:
+
+(define (mul-streams s1 s2)
+  (stream-map * s1 s2))
+
+(display "factorials;")
+(newline)
+(define factorials (cons-stream 1 (mul-streams factorials integers)))
+(display "this should print 1, 2, 6, 24, 120,... ")
+(take-n factorials 10)
+
+
+; -- streams as iterations --
+
+(define (average x y) (/ (+ x y) 2))
+
+(define (sqrt-improve guess x)
+  (average guess (/ x guess)))
+
+(define (sqrt-stream x)
+  (define guesses
+    (cons-stream 
+      1.0
+      (stream-map (lambda (guess) (sqrt-improve guess x))
+                  guesses)))
+   guesses)
+
+
+(display "approximations of root-2: ")
+(take-n (sqrt-stream 2) 7)
+
+; Exercise 3.55.  Define a procedure partial-sums that takes as argument a stream S and returns the stream whose elements are S0, S0 + S1, S0 + S1 + S2, .... For example, (partial-sums integers) should be the stream 1, 3, 6, 10, 15, .... 
+(define (partial-sums s)
+  (cons-stream
+    (stream-car s)
+    (stream-map + s (partial-sums s))))
+
+; TODO: that's not working yet
+
+(display "partial sums on integers, shld be 1 3 6 10 15:")
+(take-n (partial-sums integers) 10)
+
+
+(define (pi-summands n)
+  (cons-stream 
+    (/ 1.0 n)
+    (stream-map - (pi-summands (+ n 2)))))
+
+(define pi-stream
+  (scale-stream (partial-sums (pi-summands 1)) 4))
+
+
+(display "approximations of pi: ")
+(take-n pi-stream 10)
+
+
+; -- the end --
 (newline)
 (display "OK")
 (newline)
-
