46d9c0b96b8209e9bc34d88fe6d677da6f37d5fd
Harry <hjwp2@cantab.net>
2024-06-05 17:19:29 +0100

    lots more in chapter 2.2, working on fringe aka enumerate-tree

--- ch2.rkt
+++ ch2.rkt
@@ -16,7 +16,6 @@
 
 (check-equal? (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr eg3)))))))))))) 7)
 
-
 (define (list-ref items n)
   (if (= n 0) (car items)
     (list-ref (cdr items) (- n 1))))
@@ -38,14 +37,15 @@
       (car l1)
       (append (cdr l1) l2))))
 
+
 (check-equal? (append (list 1 2 3) (list 4 5 6)) (list 1 2 3 4 5 6))
 
 
 (define (reverse1 l) 
-  (if (null? (cdr l)) l
+  (if (null? (cdr l)) l  ; ie l has just 1 item
     (append
-      (reverse1 (cdr l))
-      (list (car l)))))
+      (reverse1 (cdr l)) ; reverse the cdr
+      (list (car l)))))  ; a list containing the car
 
 (check-equal? (reverse1 (list 1 4 9 16 25)) '(25 16 9 4 1))
 
@@ -58,13 +58,22 @@
   (if (null? (cddr l)) (list (car l))
     (cons (car l) (all-but-last (cdr l)))))
 
-(define (reverse l) 
+(define (reverse2 l) 
   (if (null? (cdr l)) l
     (cons (last-item l) (reverse (all-but-last l)))))
+
+(define (reverse l) 
+  (define (reverse-iter l acc)
+    (if (null? l) acc
+      (reverse-iter (cdr l)
+                    (cons (car l) acc))))
+
+  (reverse-iter l '()))
 
 (check-equal? (reverse '(1)) '(1))
 (check-equal? (reverse '(1 2)) '(2 1))
 
+(display "here") (newline)
 (check-equal? (reverse (list 1 4 9 16 25)) '(25 16 9 4 1))
 
 
@@ -82,12 +91,15 @@
 
 
 (define (deep-reverse tree)
-  (cond
-    ((null? tree) tree)
-    ((not (pair? tree)) tree)
-    (cons
-      (deep-reverse (cdr tree))
-      (deep-reverse (car tree)))))
+  (begin
+    (display "deep-reversing ")
+    (display tree)
+    (newline)
+    (cond
+      ((null? tree) (begin (display "null") (newline) tree))
+      ((not (pair? tree)) (begin (display "notpair") (newline) tree))
+      (else
+        (reverse (map deep-reverse tree))))))
 
 (define nestey 
   (list (list 1 2) (list 3 4)))
@@ -95,4 +107,62 @@
 (null? (cdr '(1)))
 (check-equal? (reverse nestey) '((3 4) (1 2)))
 
+(check-equal? (deep-reverse '()) '())
+(check-equal? (deep-reverse '(1 2 3)) '(3 2 1))
+(check-equal? (deep-reverse '(3 4)) '(4 3))
+
 (check-equal? (deep-reverse nestey) '((4 3) (2 1)))
+
+(cons '() '(1 2))
+
+;; first fringe solution involving an iterative helper and (append)
+;; but does the "append" make it O(n²)?
+(define (fringe1 tree)
+  (define (fringe-iter tree acc)
+    (cond
+      ((null? tree) acc)
+      ((not (pair? tree)) (cons tree acc))
+      (else
+        (append
+          (fringe-iter (car tree) acc)
+          (fringe-iter (cdr tree) '())))))
+
+  (fringe-iter tree '()))
+
+;; second fringe solution, recursive with (append) and we always return a list
+;; also O(n2) tho??
+
+(define (fringe2 tree)
+  (cond
+    ((null? tree) tree)
+    ((not (pair? tree)) (list tree))
+    (else
+      (append
+        (fringe (car tree))
+        (fringe (cdr tree))))))
+
+;; (define (first-leaf tree)
+;;   (cond
+;;     ((null? tree) tree)
+;;     ((not (pair? tree)) tree)
+;;     (first-leaf (car tree))))
+
+;; an attempt to define a more efficient recursive solution,
+;; without the worrying  (append)
+
+(define (fringe tree)
+  (define (fringe-iter t acc)
+    ((null? tree) acc)
+    ((not (pair? tree)) (cons tree acc))
+    (else
+      ;; apparently this isn't ok??
+      (fringe-iter
+        (car tree)
+        (fringe-iter (cdr tree) acc))))
+  (fringe-iter tree '()))
+
+
+(check-equal? (fringe '(1 2 3)) '(1 2 3))
+(check-equal? (fringe '((1 2) (3 4 5 (6 7 (8))) 9)) '(1 2 3 4 5 6 7 8 9))
+
+;; (check-equal? (fringe '(((1 2) (3 4)) ((1 2) (3 4)))) '(1 2 3 4 1 2 3 4))
